761832d163a58c10498ad49e64541364f3291559
RAAR-18 <raacuna@unimagdalena.edu.co>
2026-05-20 23:50:25 -0500

    (feature) añadi relación con la entidad Productor

--- src/main/java/com/agrotech/Entity/Auxiliar.java
+++ src/main/java/com/agrotech/Entity/Auxiliar.java
@@ -7,6 +7,11 @@ import java.util.List;
 @Table(name = "auxiliares")
 @PrimaryKeyJoinColumn(name = "id_usuario")
 public class Auxiliar extends Usuario{
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_productor")
+    private Productor productor;
+
     @OneToMany(mappedBy = "auxiliar", fetch = FetchType.LAZY)
     private List<EjecucionTarea> ejecucionesTareas;
 
@@ -16,6 +21,9 @@ public class Auxiliar extends Usuario{
     public void setEjecucionesTareas(List<EjecucionTarea> ejecucionesTareas) {
         this.ejecucionesTareas = ejecucionesTareas;
     }
+
+    public Productor getProductor() { return productor; }
+    public void setProductor(Productor productor) { this.productor = productor; }
 
     @Override
     public String toString() {
