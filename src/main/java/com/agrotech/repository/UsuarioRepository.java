2ffceee91b22af759c83a59e839c936609685149
RAAR-18 <raacuna@unimagdalena.edu.co>
2026-05-20 19:29:07 -0500

    (feature) elimine metodos innecesarios y agregue metodos

--- src/main/java/com/agrotech/repository/UsuarioRepository.java
+++ src/main/java/com/agrotech/repository/UsuarioRepository.java
@@ -2,8 +2,6 @@ package com.agrotech.repository;
 
 import com.agrotech.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,9 +16,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     List<Usuario> findByNombreContainingIgnoreCase(String nombre);
 
-    List<Usuario> findByRol_IdRol(Integer idRol);
+    List<Usuario> findByRol_Nombre(String nombreRol);
 
-    @Query("SELECT u FROM Usuario u WHERE u.nombre = :nombre AND u.apellido = :apellido")
-    List<Usuario> findByNombreAndApellido(@Param("nombre") String nombre,
-                                          @Param("apellido") String apellido);
+    List<Usuario> findByCorreoContainingIgnoreCase(String correo);
 }
