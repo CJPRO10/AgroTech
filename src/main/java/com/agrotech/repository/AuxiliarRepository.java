7c4f03e32dd0c1906230380d5e88b87e3cdda18e
RAAR-18 <raacuna@unimagdalena.edu.co>
2026-05-20 23:51:59 -0500

    (feature) añadi metodo buscar por productor y elimine metodos que no se usan

--- src/main/java/com/agrotech/repository/AuxiliarRepository.java
+++ src/main/java/com/agrotech/repository/AuxiliarRepository.java
@@ -1,18 +1,15 @@
 package com.agrotech.repository;
 
 import com.agrotech.Entity.Auxiliar;
+import com.agrotech.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface AuxiliarRepository extends JpaRepository<Auxiliar, Integer> {
-
-    @Query("SELECT a FROM Auxiliar a WHERE a.nombre LIKE %:nombre%")
-    List<Auxiliar> findByNombreContaining(String nombre);
-
-    @Query("SELECT a FROM Auxiliar a JOIN a.ejecucionesTareas e WHERE e.tarea.idTarea = :idTarea")
-    List<Auxiliar> findByTareaId(Integer idTarea);
+    List<Auxiliar> findByProductor_IdUsuario(Integer idProductor);
 }
